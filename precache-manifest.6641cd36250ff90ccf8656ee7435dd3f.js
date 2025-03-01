608f7d34fce2911a80988c21a1a8d96c1b6fac42
AlexCSalinas <alexsali@umich.edu>
2025-03-01 13:45:15 -0500

    manual deploy

---
+++ precache-manifest.6641cd36250ff90ccf8656ee7435dd3f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba29360f88ffd4c8e7ae6882f1c67854",
+    "url": "/personalsite/index.html"
+  },
+  {
+    "revision": "23608d4c1bc67f1df8e2",
+    "url": "/personalsite/static/css/main.9da4edd3.chunk.css"
+  },
+  {
+    "revision": "dc9027d769a913d3c6c1",
+    "url": "/personalsite/static/js/2.b0e88034.chunk.js"
+  },
+  {
+    "revision": "ba0cc13456f2d60132493643f5fc7b33",
+    "url": "/personalsite/static/js/2.b0e88034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48bc7aa89e23a61d2e99",
+    "url": "/personalsite/static/js/3.30a17b3a.chunk.js"
+  },
+  {
+    "revision": "23608d4c1bc67f1df8e2",
+    "url": "/personalsite/static/js/main.78ea5948.chunk.js"
+  },
+  {
+    "revision": "4f0aac94ab6080215e70",
+    "url": "/personalsite/static/js/runtime-main.9126a151.js"
+  },
+  {
+    "revision": "8bc4f368ac445287ecdc2170ca807932",
+    "url": "/personalsite/static/media/GCteam.8bc4f368.jpg"
+  },
+  {
+    "revision": "729244efe917214d5927c8df096363e5",
+    "url": "/personalsite/static/media/GCwhiteboard.729244ef.jpg"
+  },
+  {
+    "revision": "99de3e776a8e32321e9662e4146bbb74",
+    "url": "/personalsite/static/media/JohnMartin.99de3e77.jpg"
+  },
+  {
+    "revision": "a393df155630c39472ee4d35dc90495c",
+    "url": "/personalsite/static/media/basement.a393df15.jpg"
+  },
+  {
+    "revision": "5702761767e58c6ad5427e065e65c23f",
+    "url": "/personalsite/static/media/card.57027617.jpg"
+  },
+  {
+    "revision": "0b817325700d1b4b0b9af9586ad9b32e",
+    "url": "/personalsite/static/media/eniac.0b817325.jpg"
+  },
+  {
+    "revision": "7c169d1e6120ab02f97fb4030c8b80d7",
+    "url": "/personalsite/static/media/foot.7c169d1e.jpg"
+  },
+  {
+    "revision": "e6ac30facb46c296464af41e9706bbb9",
+    "url": "/personalsite/static/media/lovenotes.e6ac30fa.jpg"
+  },
+  {
+    "revision": "0b007f11826829009df7da2e1f48bec6",
+    "url": "/personalsite/static/media/mit.0b007f11.jpg"
+  },
+  {
+    "revision": "b948d9249ec65b874fa8a1a681acdb8e",
+    "url": "/personalsite/static/media/mitcard.b948d924.jpg"
+  },
+  {
+    "revision": "97865dcbc364064fa6dcceb325c51fab",
+    "url": "/personalsite/static/media/philly.97865dcb.jpg"
+  },
+  {
+    "revision": "ed49c8e42942fbef6d2fc334286ea2a2",
+    "url": "/personalsite/static/media/project1.ed49c8e4.jpg"
+  },
+  {
+    "revision": "b7e443fae12f21c89a87081f9bb6f2b1",
+    "url": "/personalsite/static/media/project2.b7e443fa.jpg"
+  },
+  {
+    "revision": "5677080fa635da9f34a83b252239f519",
+    "url": "/personalsite/static/media/project3.5677080f.jpg"
+  },
+  {
+    "revision": "d1a42ddb88dae2247ba40efce6fc4c96",
+    "url": "/personalsite/static/media/project4.d1a42ddb.jpg"
+  },
+  {
+    "revision": "a6058a947801acaeff8ab2889b74611f",
+    "url": "/personalsite/static/media/project5.a6058a94.jpg"
+  },
+  {
+    "revision": "47001261944a8008bc9a81404a4bdef3",
+    "url": "/personalsite/static/media/reddit.47001261.jpg"
+  },
+  {
+    "revision": "843eb840f9fc48a8c986073db8633b09",
+    "url": "/personalsite/static/media/roses.843eb840.jpg"
+  }
+]);
